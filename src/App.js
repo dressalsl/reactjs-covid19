615de4667a9da693127a7c1116cfe7120f407316
Andressa <dressalsl97@gmail.com>
2020-07-03 01:11:13 -0300

    iniciando app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className={styles.container}>
+                <Cards />
+                <CountryPicker />
+                <Chart />
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Cards } from './Cards/cards';
+export { default as Chart } from './Chart/chart';
+export { default as CountryPicker } from './CountryPicker/country-picker';
+
+//com esse export posso acessar de uma vez no App.js
